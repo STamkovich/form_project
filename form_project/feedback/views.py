33a8fdf57f5bb9d8ff585719f9a8479caf48946e
STamkovich <tamkovich1072@gmail.com>
2022-07-22 14:47:35 +0300

    'Создание формы'

---
+++ form_project/feedback/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
+
+
+def index(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/done')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
