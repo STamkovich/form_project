230fd5d79ad9888f1cb0a579cfd193158564380c
STamkovich <tamkovich1072@gmail.com>
2022-07-22 15:35:26 +0300

    'Создание формы'

--- form_project/feedback/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.core.validators import MinLengthValidator
-
-
-class Feedback(models.Model):
-    name = models.CharField(max_length=40, validators=[MinLengthValidator(3)])
-    surname = models.CharField(max_length=60)
-    feedback = models.TextField(max_length=1000)
-    rating = models.PositiveSmallIntegerField()
